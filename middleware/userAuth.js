a43a393220b0fa474229a27d289db08f8ec41dd2
Loi Van Tran <datloiboi@gmail.com>
2020-07-13 07:33:44 +0700

    Refactor

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,25 +1,21 @@
-const jwt = require('jsonwebtoken');
-const User = require("../models/User")
-
-
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
-    // make sure we get the token
-    if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer"))
-      return res.status(401).json({ status: "fail", message: "Unauthorized" });
-  
-    // verify token  
-    const token = req.headers.authorization.replace("Bearer ", "");
-    try {
-      const decoded = jwt.verify(token, process.env.SECRET);
-      // find User with token 
-      const user = await User.findOne({ _id: decoded._id });
-      if (!user) throw new Error("Unauthorized");
-  
-      // attach user object to req object
-      req.user = user;
-    } catch (err) {
-      return res.status(401).json({ status: "fail", message: err.message });
-    };
-    next();
-  };
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer")
+  )
+    return res.status(401).json({ status: "fail", message: "Unauthorized" });
+
+  const token = req.headers.authorization.replace("Bearer ", "");
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    const user = await User.findOne({ _id: decoded._id });
+    if (!user) throw new Error("Unauthorized");
+    req.user = user;
+  } catch (err) {
+    return res.status(401).json({ status: "fail", message: err.message });
+  }
+  next();
+};
--- server.js
+++ server.js
@@ -1,49 +1,41 @@
-var express = require("express")
-var cors = require("cors")
-var bodyParser = require("body-parser")
-var app = express()
-var mongoose = require("mongoose")
-const router = new express.Router()
-var port = process.env.PORT || 5000
-require("dotenv").config()
-app.use(cors())
-app.use(bodyParser.json())
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+var mongoose = require("mongoose");
+const router = new express.Router();
+var port = process.env.PORT || 5000;
+require("dotenv").config();
+app.use(cors());
+app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-    )
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-const mongoURI = 'mongodb://localhost:27017/mernloginreg'
- 
+const mongoURI = "mongodb://localhost:27017/mernloginreg";
+
 mongoose
-    .connect(
-        mongoURI, 
-        { useNewUrlParser: true }
-        )
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err))
-   
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-var users = require('./routes/UserRoute')
-app.use(router)
+var users = require("./routes/UserRoute");
+app.use(router);
 
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 router.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-    )
-router.use('/users', users)
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+router.use("/users", users);
 
-var QuestionsRoute=require('./routes/QuestionsRoute')
-const auth = require("./middleware/userAuth")
-app.use('/QuestionsRoute', auth, QuestionsRoute)
+var QuestionsRoute = require("./routes/QuestionsRoute");
+const auth = require("./middleware/userAuth");
+app.use("/QuestionsRoute", auth, QuestionsRoute);
 
-
-
-app.listen(port,() => {
-    console.log("Server is running on port:" + port)
-})
-
-
+app.listen(port, () => {
+  console.log("Server is running on port:" + port);
+});
