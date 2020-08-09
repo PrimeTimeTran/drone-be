38b997f18ea81df486d5834e5b4dd2a41f0af007
Loi Van Tran <datloiboi@gmail.com>
2020-08-09 14:58:28 +0700

    Add sending emails

--- emails/account.js
+++ emails/account.js
@@ -1,30 +1,55 @@
 const sgMail = require("@sendgrid/mail");
 const dotenv = require("dotenv");
 dotenv.config();
-
-const sendgridApiKey = process.env.SENDGRID_API_KEY
-
+const sendgridApiKey = process.env.SENDGRID_API_KEY;
 sgMail.setApiKey(sendgridApiKey);
 
-const sendWelcomeEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: 'loi@dronestudyguide.com',
-    subject: 'Welcome to our Study guide!!',
-    text: `Have fun studying ${name}!`
-  })
+const templates = {
+  register: "d-8cf7a61a05a446bfa310932fbb9decd3",
+};
+
+function sendEmail(data) {
+  const msg = {
+    to: data.receiver,
+    from: data.sender,
+    templateId: templates[data.templateName],
+    dynamic_template_data: {
+      Sender_Name: "Loi Van Tran",
+      Sender_Address: '2405 Nugget Lane',
+    },
+  };
+  sgMail.send(msg, (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("That's wassup!");
+    }
+  });
 }
+
+const sendWelcomeEmail = (email, name) => {
+  const data = {
+    name,
+    receiver: email,
+    templateName: "register",
+    sender: "loi@dronestudyguide.com",
+    dynamic_template_data: {
+      name,
+   }
+  };
+  sendEmail(data)
+};
 
 const accountDeletionFollowupEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'loi@dronestudyguide.com',
+    from: "loi@dronestudyguide.com",
     subject: "So sad we've lost you",
-    text: `Thank you for using our service ${name}. We sent a short survey to learn what we could do to make you want to stay. We'd appreciate it if you'd answer our question!`
-  })
-}
+    text: `Thank you for using our service ${name}. We sent a short survey to learn what we could do to make you want to stay. We'd appreciate it if you'd answer our question!`,
+  });
+};
 
 module.exports = {
   sendWelcomeEmail,
-  accountDeletionFollowupEmail
-}
+  accountDeletionFollowupEmail,
+};
