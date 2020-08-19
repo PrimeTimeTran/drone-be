bdee97e6c1ca83196222503ede53d978d4da9d6f
Loi Van Tran <datloiboi@gmail.com>
2020-08-19 10:16:33 +0700

    Update db URL

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI_ATLAS, { useNewUrlParser: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
